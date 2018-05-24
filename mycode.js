5edd9ece120ed9c68f502aded498b646383d4a13
Diego <diegoazd@hotmail.com>
2018-05-23 21:42:56 -0500

    Add koans

--- mycode.js
+++ mycode.js
@@ -1,3 +1,19 @@
 var sum = function(a, b) {
   return a + b;
 }
+
+var returnTruthy = function() {
+  return 0;
+}
+
+var returnFalsy = function() {
+  return '0';
+}
+
+var returnCoartionValue = function() {
+  return '2';
+}
+
+var returnStrictEqualValue = function() {
+  return 2;
+}
--- test.example.js
+++ test.example.js
@@ -1,7 +1,27 @@
 var assert = chai.assert;
 
 describe('My first test', function(){
-  it('Should be equals 2+2 ==4', function(){
-    assert.equal(sum(2,2), 4);
+  it('Should sum 2 numbers', function(){
+    assert.equal(sum(2,2), __, 'Cuanto es 2 +2?');
+  });
+
+  it('Should assert array', function() {
+    assert(Array.isArray(__), 'Agrega un arreglo');
+  });
+
+  it('Should assert a truthy', function() {
+    assert.isOk(returnTruthy(), 'Agrega un truthy');
+  });
+
+  it('Should assert falsy', function() {
+    assert.isNotOk(returnFalsy(), 'Agrega un falsy');
+  });
+
+  it('Should assert with not strict-equality', function() {
+    assert.equal(__, returnCoartionValue(), 'Agrega el equivalente en js para que haga coercion');
+  });
+
+  it('Should assert with strict equility', function() {
+    assert.strictEqual(2, returnStrictEqualValue, 'Agrega un valor igual')
   });
 });
